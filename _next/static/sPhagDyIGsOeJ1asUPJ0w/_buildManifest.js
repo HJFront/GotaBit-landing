9b0663c6d5858df27a741e54734f4d3b238ee9c9
WuChenDi <wuchendi96@gmail.com>
2022-08-08 07:33:09 +0000

    Deploying to gh-pages from @ HJFront/GotaBit-landing@b34d59a8f9172ee695be1532c6b985d13df4041f 🚀

---
+++ _next/static/sPhagDyIGsOeJ1asUPJ0w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/309-887eb6904218a2e7.js","static/css/28bc8960cd363da6.css","static/chunks/pages/index-d3b33a42c5c76f61.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
